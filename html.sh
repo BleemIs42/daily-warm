3d7c5cbf078e11b463f097e457117e51f049f4f1
Barry Yan <barry.yansh@gmail.com>
2019-10-22 12:09:42 +0800

    Merge html.sh to test.sh

--- html.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\
-package main
-
-// HTML for email template
-const HTML = \`
-$(cat email.html)
-\`"  > html.go 
--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+echo -e "\
+package main
+
+// HTML for email template
+const HTML = \`
+$(cat email.html)
+\`"  > html.go 
+
 go run *.go > test.html
+
+
